364ff238149955f3c7c7e409d2d42c77c3e7f54f
Станислав Терещенков <tereschenkov.s@ati.su>
2024-03-26 15:02:16 +0300

    fix di

--- ATI.Services.Consul/ConsulMetricsHttpClientWrapperServiceCollectionExtensions.cs
+++ ATI.Services.Consul/ConsulMetricsHttpClientWrapperServiceCollectionExtensions.cs
@@ -8,6 +8,8 @@ public static class ConsulMetricsHttpClientWrapperServiceCollectionExtensions
 {
     public static IServiceCollection AddConsulMetricsHttpClientWrappers(this IServiceCollection services)
     {
+        // Add IHttpClientFactory for ConsulMetricsHttpClientWrapper<>
+        services.AddHttpClient();
         services.AddSingleton(typeof(ConsulMetricsHttpClientWrapper<>));
         return services;
     }
