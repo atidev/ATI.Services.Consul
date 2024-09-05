da30775846052a1325c819a148b6003b02fdaa5a
Aidar Shaikhiev <muphblu09@gmail.com>
2024-09-05 20:42:48 +0500

    Consul deregistration using hosted service

---
+++ ATI.Services.Consul/ConsulHostedService.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ATI.Services.Common.Behaviors;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace ATI.Services.Consul;
+
+[PublicAPI]
+public class ConsulHostedService(
+    IOptions<ConsulRegistratorOptions> consulRegistratorOptions,
+    ConsulRegistrator registrator) : IHostedService
+{
+    public Task StartAsync(CancellationToken ct)
+    {
+        if (bool.TryParse(ConfigurationManager.AppSettings("ConsulEnabled"), out var enabled) && enabled)
+            return registrator.RegisterServicesAsync(consulRegistratorOptions.Value,
+                                                     ConfigurationManager.GetApplicationPort());
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken ct) => registrator.DeregisterInstanceAsync();
+}
--- ATI.Services.Consul/ConsulRegistrator.cs
+++ ATI.Services.Consul/ConsulRegistrator.cs
@@ -7,93 +7,93 @@ using Consul;
 using Newtonsoft.Json;
 using NLog;
 
-namespace ATI.Services.Consul
-{
-    public static class ConsulRegistrator
-    {
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-        private static Timer _reregistrationTimer;
-        private static HashSet<string> RegisteredServices { get; set; } = new();
+namespace ATI.Services.Consul;
 
-        public static async Task RegisterServicesAsync(ConsulRegistratorOptions consulRegistratorOptions, int applicationPort)
+public class ConsulRegistrator
+{
+    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+    private Timer _reregistrationTimer;
+    private readonly HashSet<string> _registeredServices = [];
+
+    public async Task RegisterServicesAsync(ConsulRegistratorOptions consulRegistratorOptions, int applicationPort)
+    {
+        foreach (var consulServiceOptions in consulRegistratorOptions.ConsulServiceOptions)
+        {
+            consulServiceOptions.Check.HTTP = $"http://localhost:{applicationPort}{consulServiceOptions.Check.HTTP}";
+            await DeregisterFromConsulAsync($"{consulServiceOptions.ServiceName}-{Dns.GetHostName()}-{applicationPort}");
+        }
+
+        if(_reregistrationTimer != null) 
+            await _reregistrationTimer.DisposeAsync();
+        
+        _reregistrationTimer = new Timer(async _ => await RegisterServicesAsyncPrivate(consulRegistratorOptions, applicationPort), 
+                                         null, 
+                                         TimeSpan.FromSeconds(0), 
+                                         consulRegistratorOptions.ReregistrationPeriod);
+    }
+
+    private async Task RegisterServicesAsyncPrivate(ConsulRegistratorOptions consulRegistratorOptions, int applicationPort)
+    {
+        try
         {
             foreach (var consulServiceOptions in consulRegistratorOptions.ConsulServiceOptions)
-            {
-                consulServiceOptions.Check.HTTP = $"http://localhost:{applicationPort}{consulServiceOptions.Check.HTTP}";
-                await DeregisterFromConsulAsync($"{consulServiceOptions.ServiceName}-{Dns.GetHostName()}-{applicationPort}");
-            }
-
-            _reregistrationTimer = new Timer(async _ => await RegisterServicesAsyncPrivate(consulRegistratorOptions, applicationPort), 
-                null, 
-                TimeSpan.FromSeconds(0), 
-                consulRegistratorOptions.ReregistrationPeriod);
+                await RegisterToConsulAsync(consulServiceOptions, applicationPort);
         }
-
-        private static async Task RegisterServicesAsyncPrivate(ConsulRegistratorOptions consulRegistratorOptions, int applicationPort)
+        catch (Exception e)
         {
-            try
-            {
-                foreach (var consulServiceOptions in consulRegistratorOptions.ConsulServiceOptions)
-                    await RegisterToConsulAsync(consulServiceOptions, applicationPort);
-            }
-            catch (Exception e)
-            {
-                Logger.Error(e);
-            }
+            _logger.Error(e);
         }
+    }
         
-        private static async Task RegisterToConsulAsync(ConsulServiceOptions options, int applicationPort)
-        {
-            var serviceId = $"{options.ServiceName}-{Dns.GetHostName()}-{applicationPort}";
-            RegisteredServices.Add(serviceId);
+    private async Task RegisterToConsulAsync(ConsulServiceOptions options, int applicationPort)
+    {
+        var serviceId = $"{options.ServiceName}-{Dns.GetHostName()}-{applicationPort}";
+        _registeredServices.Add(serviceId);
 
-            var swaggerUrls = JsonConvert.SerializeObject(options.SwaggerUrls);
+        var swaggerUrls = JsonConvert.SerializeObject(options.SwaggerUrls);
             
+        using var client = new ConsulClient();
+        var cr = new AgentServiceRegistration
+        {
+            Name = options.ServiceName,
+            ID = serviceId,
+            Tags = options.Tags,
+            Check = options.Check,
+            Port = applicationPort,
+            Meta = new Dictionary<string, string>
+            {
+                {"swagger_urls", swaggerUrls}
+            }
+        };
+        await client.Agent.ServiceRegister(cr);
+    }
+
+    public async Task DeregisterInstanceAsync()
+    {
+        await _reregistrationTimer.DisposeAsync();
+        try
+        {
+            foreach (var serviceId in _registeredServices)
+            {
+                await DeregisterFromConsulAsync(serviceId);
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.Error(e);
+        }
+    }
+
+    private async Task DeregisterFromConsulAsync(string serviceId)
+    {
+        try
+        {
             using var client = new ConsulClient();
-            var cr = new AgentServiceRegistration
-            {
-                Name = options.ServiceName,
-                ID = serviceId,
-                Tags = options.Tags,
-                Check = options.Check,
-                Port = applicationPort,
-                Meta = new Dictionary<string, string>
-                {
-                    {"swagger_urls", swaggerUrls}
-                }
-            };
-            await client.Agent.ServiceRegister(cr);
+            await client.Agent.ServiceDeregister(serviceId);
         }
-
-        public static async Task DeregisterInstanceAsync()
+        catch (Exception ex)
         {
-            await _reregistrationTimer.DisposeAsync();
-            try
-            {
-                foreach (var serviceId in RegisteredServices)
-                {
-                    await DeregisterFromConsulAsync(serviceId);
-                }
-            }
-            catch (Exception e)
-            {
-                Logger.Error(e);
-            }
+            _logger.Error(ex, $"Не удалось дерегистрировать {serviceId} из консула.");
         }
-
-        private static async Task DeregisterFromConsulAsync(string serviceId)
-        {
-            try
-            {
-                using var client = new ConsulClient();
-                await client.Agent.ServiceDeregister(serviceId);
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex, $"Не удалось дерегистрировать {serviceId} из консула.");
-            }
-        }
-
-
     }
 }
